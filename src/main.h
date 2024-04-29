be98982c5cdac8365ff4f374a20318e2820c554b
Leo Lin <44302577+LeoLinRui@users.noreply.github.com>
2024-04-29 15:03:09 -0400

    added LocalMesh struct

--- src/main.h
+++ src/main.h
@@ -1,5 +1,6 @@
 #include <Fade_2D.h>
 #include <stdio.h>
+#include <optional>
 
 using namespace GEOM_FADE2D;
 
@@ -9,6 +10,63 @@ struct RuntimeParameters {
     int numNodes;
     MeshGenParams meshGenParams;
     Fade_2D mesh;
+};
+
+struct LocalMesh {
+    // Triangulation
+    Fade_2D mesh;
+
+    // Zones - Main
+    Zone2* Main_TL;
+    Zone2* Main_TR;
+    Zone2* Main_BL;
+    Zone2* Main_BR;
+
+    // Zones - Inner Edges
+    Zone2* Inner_Top;
+    Zone2* Inner_Bottom;
+    Zone2* Inner_Right;
+    Zone2* Inner_Left;
+
+    // Zones - Outer Edges
+    Zone2* Outer_Top;
+    Zone2* Outer_Top_L;
+    Zone2* Outer_Top_R;
+
+    Zone2* Outer_Bottom;
+    Zone2* Outer_Bottom_L;
+    Zone2* Outer_Bottom_R;
+
+    Zone2* Outer_Right;
+    Zone2* Outer_Right_T;
+    Zone2* Outer_Right_B;
+
+    Zone2* Outer_Left;
+    Zone2* Outer_Left_T;
+    Zone2* Outer_Left_B;
+
+    // Zones - Corners
+    Zone2* Inner_TL;
+    Zone2* Inner_TR;
+    Zone2* Inner_BL;
+    Zone2* Inner_BR; 
+
+    Zone2* Outer_TL;
+    Zone2* Outer_TR;
+    Zone2* Outer_BL;
+    Zone2* Outer_BR; 
+
+    // Neighboring meshes
+    std::optional<int> Neighbor_Left;
+    std::optional<int> Neighbor_Right;
+    std::optional<int> Neighbor_Top;
+    std::optional<int> Neighbor_Bottom;
+
+    std::optional<int> Neighbor_TL;
+    std::optional<int> Neighbor_TR;
+    std::optional<int> Neighbor_BL;
+    std::optional<int> Neighbor_BR;
+
 };
 
 enum Phase {
